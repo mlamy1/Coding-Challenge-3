8d59ef55564f3de6e8ff1f2b5a62de070d0dec0f
Myia Lamy <mlamy@usf.edu>
2024-09-08 15:36:21 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -12,3 +12,21 @@ function calculateAverageSales(sales) {
  let sales = [ 1000, 2000, 3000, 4000];  // Example numbers 
  console.log("Average Sales: ", calculateAverageSales(sales)); 
 
+// Task 2: Create a Function to Determine Performance Rating
+ 
+let averageSales = calculateAverageSales(sales); 
+
+function determinePerformanceRating(averageSales) {
+    if (averageSales > 10000) {
+        return "Excellent";
+    }  else if(averageSales >= 7000) {
+        return "Good";
+    }  else if (averageSales >= 4000) {
+        return "Satisfactory";
+    } else {
+        return "Needs Improvement";
+    }
+
+} 
+// Displays performance rating based on average sales.
+console.log("Performance Rating:", determinePerformanceRating(averageSales)); 
